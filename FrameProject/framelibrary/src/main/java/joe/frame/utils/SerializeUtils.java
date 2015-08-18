00c4b1c83f0662247d1d19f7d65d0e3638bb7010
Joe Chan <1030310877@qq.com>
2015-08-18 09:30:26 +0800

    添加序列化工具类

---
+++ FrameProject/framelibrary/src/main/java/joe/frame/utils/SerializeUtils.java
@@ -0,0 +1,133 @@
+package joe.frame.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Serialize Utils
+ *
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2012-5-14
+ */
+public class SerializeUtils {
+
+    private SerializeUtils() {
+        throw new AssertionError();
+    }
+
+    /**
+     * deserialization from file
+     *
+     * @param filePath
+     * @return
+     * @throws RuntimeException if an error occurs
+     */
+    public static Object deserialization(String filePath) {
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new FileInputStream(filePath));
+            Object o = in.readObject();
+            in.close();
+            return o;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("FileNotFoundException occurred. ", e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("ClassNotFoundException occurred. ", e);
+        } catch (IOException e) {
+            throw new RuntimeException("IOException occurred. ", e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("IOException occurred. ", e);
+                }
+            }
+        }
+    }
+
+    /**
+     * serialize to file
+     *
+     * @param filePath
+     * @param obj
+     * @return
+     * @throws RuntimeException if an error occurs
+     */
+    public static void serialization(String filePath, Object obj) {
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream(filePath));
+            out.writeObject(obj);
+            out.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("FileNotFoundException occurred. ", e);
+        } catch (IOException e) {
+            throw new RuntimeException("IOException occurred. ", e);
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("IOException occurred. ", e);
+                }
+            }
+        }
+    }
+
+    /**
+     * Object转byte数组，Object必须实现序列化{@link java.io.Serializable}
+     *
+     * @param object
+     * @return
+     */
+    public static byte[] toBytes(Object object) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            return baos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * byte数组转对象
+     *
+     * @param bytes
+     * @return
+     */
+    public static Object toObject(byte[] bytes) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
