0f1499d8413a211f4b70ef385b2a1f561268d702
Joe Chan <1030310877@qq.com>
2015-11-11 18:42:42 +0800

    添加wifi连接工具类

---
+++ FrameProject/framelibrary/src/main/java/joe/frame/utils/WifiUtils.java
@@ -0,0 +1,197 @@
+package joe.frame.utils;
+
+import android.content.Context;
+import android.net.DhcpInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import java.util.List;
+
+/**
+ * Description  Wifi设置工具类
+ * Created by chenqiao on 2015/11/11.
+ */
+public class WifiUtils {
+    private WifiManager mWifiManager;
+    private WifiInfo mWifiInfo;
+    private List<ScanResult> mWifiList = null;
+    private List<WifiConfiguration> mWifiConfiguration;
+    private WifiManager.WifiLock mWifiLock;
+    private DhcpInfo dhcpInfo;
+
+    public WifiUtils(Context context) {
+        mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        mWifiInfo = mWifiManager.getConnectionInfo();
+    }
+
+    public boolean openWifi() {//打开wifi
+        if (!mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(true);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return mWifiManager.isWifiEnabled();
+    }
+
+    public void closeWifi() {
+        if (mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(false);
+        }
+    }
+
+    public int checkState() {
+        return mWifiManager.getWifiState();
+    }
+
+    public void acquireWifiLock() {//锁定wifiLock
+        mWifiLock.acquire();
+    }
+
+    public void releaseWifiLock() {//解锁wifiLock
+        if (mWifiLock.isHeld()) {
+            mWifiLock.acquire();
+        }
+    }
+
+    public void creatWifiLock() {
+        mWifiLock = mWifiManager.createWifiLock("Test");
+    }
+
+    public List<WifiConfiguration> getConfiguration() {
+        return mWifiConfiguration;
+    }
+
+    public void connectConfiguration(int index) {//指定配置好的网络进行连接
+        if (index > mWifiConfiguration.size()) {
+            return;
+        }
+        mWifiManager.enableNetwork(mWifiConfiguration.get(index).networkId, true);
+    }
+
+    public void startScan() {//wifi扫描
+        boolean scan = mWifiManager.startScan();
+        mWifiList = mWifiManager.getScanResults();
+        mWifiConfiguration = mWifiManager.getConfiguredNetworks();
+
+        if (mWifiList != null) {
+            for (int i = 0; i < mWifiList.size(); i++) {
+                ScanResult result = mWifiList.get(i);
+            }
+        } else {
+        }
+
+    }
+
+    public List<ScanResult> getWifiList() {
+        return mWifiList;
+    }
+
+    public StringBuilder lookUpScan() {// 查看扫描结果
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < mWifiList.size(); i++) {
+            stringBuilder.append("Index_" + new Integer(i + 1).toString() + ":");
+            stringBuilder.append((mWifiList.get(i)).toString());
+            stringBuilder.append("/n");
+        }
+        return stringBuilder;
+    }
+
+    public String getMacAddress() {
+        return (mWifiInfo == null) ? "NULL" : mWifiInfo.getMacAddress();
+    }
+
+    public String getBSSID() {
+        return (mWifiInfo == null) ? "NULL" : mWifiInfo.getBSSID();
+    }
+
+    public DhcpInfo getDhcpInfo() {
+        return dhcpInfo = mWifiManager.getDhcpInfo();
+    }
+
+    public int getIPAddress() {
+        return (mWifiInfo == null) ? 0 : mWifiInfo.getIpAddress();
+    }
+
+    public int getNetworkId() {
+        return (mWifiInfo == null) ? 0 : mWifiInfo.getNetworkId();
+    }
+
+    public WifiInfo getWifiInfo() {
+        mWifiInfo = mWifiManager.getConnectionInfo();
+        return mWifiInfo;
+    }
+
+    public void addNetwork(WifiConfiguration wcg) { // 添加一个网络配置并连接
+        int wcgID = mWifiManager.addNetwork(wcg);
+        boolean b = mWifiManager.enableNetwork(wcgID, true);
+        System.out.println("addNetwork--" + wcgID);
+        System.out.println("enableNetwork--" + b);
+    }
+
+    public void disconnectWifi(int netId) {
+        mWifiManager.disableNetwork(netId);
+        mWifiManager.disconnect();
+    }
+
+    public WifiConfiguration CreateWifiInfo(String SSID, String Password, int Type) {
+        WifiConfiguration config = new WifiConfiguration();
+        config.allowedAuthAlgorithms.clear();
+        config.allowedGroupCiphers.clear();
+        config.allowedKeyManagement.clear();
+        config.allowedPairwiseCiphers.clear();
+        config.allowedProtocols.clear();
+        config.SSID = "\"" + SSID + "\"";
+
+        WifiConfiguration tempConfig = this.IsExsits(SSID);
+
+        if (tempConfig != null) {
+            mWifiManager.removeNetwork(tempConfig.networkId);
+        } else {
+        }
+
+        if (Type == 1) { // WIFICIPHER_NOPASS
+            config.wepKeys[0] = "";
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+        }
+        if (Type == 2) {// WIFICIPHER_WEP
+            config.hiddenSSID = true;
+            config.wepKeys[0] = "\"" + Password + "\"";
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            config.wepTxKeyIndex = 0;
+        }
+        if (Type == 3) { // WIFICIPHER_WPA
+            config.preSharedKey = "\"" + Password + "\"";
+            config.hiddenSSID = true;
+            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            // config.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+            config.status = WifiConfiguration.Status.ENABLED;
+        }
+        return config;
+    }
+
+    private WifiConfiguration IsExsits(String SSID) { // 查看以前是否已经配置过该SSID
+        List<WifiConfiguration> existingConfigs = mWifiManager.getConfiguredNetworks();
+        for (WifiConfiguration existingConfig : existingConfigs) {
+            if (existingConfig.SSID.equals("\"" + SSID + "\"")) {
+                return existingConfig;
+            }
+        }
+        return null;
+    }
+}
