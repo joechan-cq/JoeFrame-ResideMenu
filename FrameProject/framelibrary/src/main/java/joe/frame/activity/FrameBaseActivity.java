ca02c95219b8ade89e8c77e5bfbb30cdfb679d47
Joe Chan <1030310877@qq.com>
2016-09-02 10:36:14 +0800

    全屏模式

--- FrameProject/framelibrary/src/main/java/joe/frame/activity/FrameBaseActivity.java
+++ FrameProject/framelibrary/src/main/java/joe/frame/activity/FrameBaseActivity.java
@@ -288,6 +288,22 @@ public abstract class FrameBaseActivity extends AppCompatActivity {
         }
     }
 
+    public void setFullScreen(boolean isFullScreen) {
+        if (isFullScreen) {
+            hideToolbar();
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            );
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
+    }
+
     /**
      * 侧滑菜单相关
      */
